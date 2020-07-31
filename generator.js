7397b6a87f08f6fa4ae680d80c41e8f413c52ddc
Katharina Kloppenborg <katharina.kloppenborg@gmail.com>
2020-08-01 00:35:57 +0200

    Update text generator
    Added a sourcefile that contains ~140 short phrases (texts.txt). First letters of sentences automatically get changed to upper case. Punctuation marks are chosen randomly from an array (only the last punctuation mark is always '.'

--- generator.js
+++ generator.js
@@ -1,18 +1,17 @@
 // Code that generates a random lorem ipsum text
 
+// read source text into array
+var fs = require('fs');
+var doggoarray = fs.readFileSync('texts.txt').toString().split("\n");
+
 // Create a new object called loremIpsum by invoking the GenerateNewText constructor function
 const loremIpsum = new GenerateNewText();
 
 // Constructor function that creates an object with the sentences property
 function GenerateNewText() {
     // Add property to the object
-    this.sentences = 
-        [
-           "Bork bork bork!",
-           "Bestest boi.",
-           "Moon moon.",
-           "Many nom nom!" 
-        ];
+    this.sentences = doggoarray;
+    this.punctuation = [".", ",", "!", "?", ".", ",", ",", "!!"];
 }
 
 // Method to the GenerateNewText constructor function that generates a random sentence
@@ -25,16 +24,25 @@ GenerateNewText.prototype.getRandomSentence = function() {
 GenerateNewText.prototype.getParagraph = function() {
     let paragraph = "";
     // Set the minimum number of words
-    let minimumCharacterLength = 250;
+    let minimumCharacterLength = 500;
     let firstSentence = true;
     while (paragraph.length < minimumCharacterLength) {
+        sentence = this.getRandomSentence();
         if(firstSentence) {
-            paragraph = paragraph.concat(this.getRandomSentence());
+            // choose a random element from the sentences-array, turn the first letter to upper case and add a random punctuation mark
+            paragraph = paragraph.concat(sentence.charAt(0).toUpperCase() + sentence.slice(1) + this.punctuation[Math.floor(Math.random() * this.punctuation.length)]); 
             firstSentence = false;
         } else {
-            paragraph = paragraph.concat(" " + this.getRandomSentence());
+            if (paragraph.slice(-1) == ",") {
+                paragraph = paragraph.concat(" " + sentence + this.punctuation[Math.floor(Math.random() * this.punctuation.length)]);
+            } else {
+                // Turn first letter to upper case if punctuation mark before is not ","
+                paragraph = paragraph.concat(" " + sentence.charAt(0).toUpperCase() + sentence.slice(1) + this.punctuation[Math.floor(Math.random() * this.punctuation.length)]);
+            }
+            
         }
     }
+    paragraph = paragraph.slice(0, -1) + ".";
     return paragraph;
 }
 
