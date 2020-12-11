37c2b0dac751a708d23f4a430c406d112137578d
katoss <katharina.kloppenborg@stud.uni-due.de>
2020-12-11 23:53:04 +0100

    fix error try

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.use(routes);
 
 // Begin accepting connections to the specified port
-app.listen(PORT, () => {
+app.listen(port, () => {
     // Display server location information to the console
-    console.log('App running on port ${PORT}!');
+    console.log('Server is listening at http://'+ hostname + ':' + port + '/');
 })
